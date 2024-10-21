b4a0b29ab410c02ed3baa2935eb47acb309a79fa
saranya <saranyanambi911@gmail.com>
2024-10-21 10:24:03 +0530

    state selection implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
+
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState("");
+  const [states, setStates] = useState([]);
+  const [selectedState, setSelectedState] = useState("");
+  const [cities, setCities] = useState([]);
+  const [selectedCity, setSelectedCity] = useState("");
+  const [statedisable,setStatedisable]=useState(true);
+  const [citydisable,setCitydisable]=useState(true);
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      const res = await fetch("https://crio-location-selector.onrender.com/countries");
+      const data = await res.json();
+      setCountries(data);
+    };
+
+    fetchCountries();
+  }, []);
+
+  const handleCountryChange = async (e) => {
+    const country = e.target.value;
+    setSelectedCountry(country);
+    setStatedisable(false)
+    // setCitydisable(!citydisable)
+    const res = await fetch(`https://crio-location-selector.onrender.com/country=${country}/states`);
+    const data = await res.json();
+    setStates(data);
+    setSelectedState("");
+    setCities([]);
+  };
+
+  const handleStateChange = async (e) => {
+    const stateValue = e.target.value;
+    setSelectedState(stateValue);
+    setCitydisable(false)
+    
+    
+    const res = await fetch(`https://crio-location-selector.onrender.com/country=${selectedCountry}/state=${stateValue}/cities`);
+    const data = await res.json();
+    setCities(data);
+  
+    setSelectedCity("");
+  };
+
+  const handleCityChange = (e) => {
+    setSelectedCity(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Select Location</h1>
+     
+        <select onChange={handleCountryChange} value={selectedCountry}>
+          <option value="">Select a Country</option>
+          {countries.map((item) => (
+            <option value={item} key={item}>{item}</option>
+          ))}
+        </select>
+      
+
+     
+        <select disabled={statedisable} onChange={handleStateChange} value={selectedState}>
+          <option value="">Select State</option>
+          {states.map((item) => (
+            <option value={item} key={item}>{item}</option>
+          ))}
+        </select>
+      
+        
+      
+
+     
+        <select disabled={citydisable} onChange={handleCityChange} value={selectedCity}>
+          <option value="">Select City</option>
+          {cities.map((item) => (
+            <option value={item} key={item}>{item}</option>
+          ))}
+        </select>
+      
+
+      {selectedCountry && selectedState && selectedCity && (
+        <h4>You Selected <strong>{selectedCountry}</strong> , <span style={{"color":"grey"}}>{selectedState} , {selectedCity} </span></h4>
+      )}
     </div>
   );
 }
